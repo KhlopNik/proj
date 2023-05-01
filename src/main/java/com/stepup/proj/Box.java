215f0c236b225c59b53f402f7dde0c4e2b5d7bc6
Nikita Khlopotov <KhlopotovNS@ya.ru>
2023-05-01 09:30:05 +0300

    Изменена основня логика Добавлен класс коробки

---
+++ src/main/java/com/stepup/proj/Box.java
@@ -0,0 +1,16 @@
+package com.stepup.proj;
+
+public class Box {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public Box() {
+    }
+
+    public Box(int size) {
+        this.size = size;
+    }
+}
